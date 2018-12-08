286e57ce7e56ac0dcb8a8e0037d0e360bcad7fc1
Chris Malcolm <chrisishere@gmail.com>
2018-12-08 14:08:42 -0800

    [init] proxy server setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Chris's Proxy Service
+
+Proxy service for 9Trails. TO launch
+
+``` sh
+# install if you haven't yet 
+$> npm install
+# run 
+$> npm run server-dev
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use('/', express.static(__dirname + '/public/'));
+// because we use layout.html...im going to just serve that as root
+app.get('/', (req, res) => {
+  res.status(200).sendFile(__dirname + '/public/layout.html');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`listen on ${PORT}...`);
+});
+
